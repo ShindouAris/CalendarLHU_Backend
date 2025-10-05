445bf128786134ee7094b169e89f844eaca5e587
Chisadin <tranthanhnam.ccc@gmail.com>
2025-10-05 14:59:11 +0700

    Implement Cloudflare token verification and update login endpoint to include verification

---
+++ src/utils/cloudflare.ts
@@ -0,0 +1,35 @@
+import { fetch } from "bun";
+
+const apiCF = "https://challenges.cloudflare.com/turnstile/v0/siteverify"
+
+const secKey = process.env.CF_SECRET_KEY || ""
+
+export const verfiyToken = async (token: string, remoteip?: string): Promise<boolean> => {
+    try {
+        if (!token) {
+            return false
+        }
+
+        const payload = new FormData();
+        payload.append('secret', secKey);
+        payload.append('response', token);
+        if (remoteip) {
+            payload.append('remoteip', remoteip);
+        }
+
+        const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+              method: 'POST',
+              body: payload
+          });
+
+          const result = await response.json();
+          if (!result.success) {
+            console.error('Turnstile validation failed:', result['error-codes']);
+            return false;
+          }
+
+          return true;
+    } catch (error) {
+        console.error('Turnstile validation error:', error);
+        return false;}
+}
