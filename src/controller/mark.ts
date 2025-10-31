d17b870441563a37bca4f8ebecbf792fcf7f37b3
Chisadin <tranthanhnam.ccc@gmail.com>
2025-11-01 00:31:11 +0700

    Planned: Remove Deprecated API

--- src/controller/mark.ts
+++ src/controller/mark.ts
@@ -23,65 +23,13 @@ const api_mark = process.env.API_MARK || ""
 async function getGradesGrouped(accessToken: string): Promise<HocKyGroup | null> {
     try {
 
-        const cookies = `awt=${accessToken}`
-
-        const data  = await fetch(api_mark, {
-            method: "GET",
-            headers: {
-                'Cookie': cookies,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            }
-        });
-
-        const html = await data.text()
-
-        if (!html) {
-            throw new Error("No html found")
-        } 
-
-        // fs.writeFileSync("mark.html", html) // debug lines
-
-        const $ = load(html);
         const groupedGrades: HocKyGroup = {
             semesters: {},
             tin_chi_tich_luy: 0,
         };
-        
-        let so_tin_chi_tich_luy: number = 0
-
-        // TODO:Phát hiện div Không cho xem điểm
-        const blocked = $('#KhongChoXemDiem')
-        if (blocked.length > 0) {
-            const masinhvien = $('#LbMaSV').text().trim()
-            console.warn(`Cảnh báo: User ${masinhvien} không xem điểm được`)
-            groupedGrades.reason = blocked.text().trim()
-        }
-
-        // Lấy điểm từ bảng
-        $('#tblBangDiem tbody').slice(1).each((_:any, tbody:any) => {
-            const semesterName = $(tbody).find('tr').first().find('td.RowGroup').text().trim().replace("Học kỳ ", "");
-
-            const courses: MonHoc[] = [];
-
-            $(tbody).find('tr').slice(1).each((_:any, row:any) => {
-                const cells = $(row).find('td');
-                if (cells.length === 6) {
-                    courses.push({
-                        ma_mon_hoc: $(cells[0]).text().trim(),  
-                        ten_mon_hoc: $(cells[1]).text().trim(),
-                        he_so: $(cells[2]).text().trim(), // -> tín chỉ / học kì
-                        diem_thanh_phan: $(cells[3]).text().trim() || "Chưa có",
-                        diem_trung_binh: $(cells[4]).text().trim() || "Chưa có",
-                    });
-                    so_tin_chi_tich_luy += Number($(cells[2]).text().trim()) || 0
-                }
-            });
-
-            groupedGrades.semesters[semesterName] = courses;
-        });
-
-        groupedGrades.tin_chi_tich_luy = so_tin_chi_tich_luy
-
+        groupedGrades.reason = "Phiên bản LHU Dashboard của bạn đã cũ, hãy cập nhật lên phiên bản mới"
+        groupedGrades.semesters = {};
+        groupedGrades.tin_chi_tich_luy = 0
         return groupedGrades;
 
     } catch (err) {
